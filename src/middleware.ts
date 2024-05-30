f1ba8c80835ba4f15a3422d80672a818dbf7380c
Engraya <ahmadyakubuaya@gmail.com>
2024-05-30 01:48:07 +0100

    Added Environment Variable

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes : ['api/webhooks/clerk']
+// export default clerkMiddleware({
+//   publicRoutes : ['api/webhooks/clerk']
+// });
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
 
 export const config = {
