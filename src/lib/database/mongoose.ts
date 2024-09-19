9c80daa5a7b476d72ecfa2f104fa5a0a9aba8010
Engraya <ahmadyakubuaya@gmail.com>
2024-09-19 14:07:50 +0100

    Changed Database Directory Name

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'snappsavvyPro_DB', bufferCommands: false 
+      dbName: 'Snap_Database', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
